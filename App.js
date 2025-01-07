02e08f993265831aa0dd4bb6627b71b48cf5dbd1
manojkumardash93 <96936164+manojkumardash93@users.noreply.github.com>
2025-01-07 21:34:57 +0530

    End of Ep3

--- App.js
+++ App.js
@@ -1,2 +1,23 @@
 import React from "react";
-import ReactDom from "react-dom"
+import ReactDOM from "react-dom/client"; // Correct import for ReactDOM in React 18+
+ 
+// const jsxHeading = <h1>Namaste React using JSX!</h1>;
+const Title = () => {
+    return <h1>Namaste React from JSX</h1>
+}
+
+const element = (
+    <h1>React Eliment is Here</h1>
+);
+const HeadingComponet = () =>(
+    <div id="container">
+    <Title/>
+    {element}
+    <h1 className = "head">Namaste React from Componet</h1>
+    </div>
+    
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root")); // Create a root for rendering
+ 
+root.render(<HeadingComponet/>); // Render the JSX heading
